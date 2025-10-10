29b090b6c9a70076cf3bbd0f562d6600ffe5494b
Wyatt McCurdy <36129149+wyattmccurdy12@users.noreply.github.com>
2025-10-10 18:10:45 -0400

    update training sh for r34

--- sh_commands/run_train_and_test_resnet.sh
+++ sh_commands/run_train_and_test_resnet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py -t fe -b 32 -n 30 -m resnet50
-python train.py -t fs -b 32 -n 30 -m resnet50
-python train.py -t i2i -b 32 -n 30 -m resnet50
-python train.py -t t2i -b 32 -n 30 -m resnet50
+python train.py -t fe -b 32 -m resnet34
+python train.py -t fs -b 32 -m resnet34
+python train.py -t i2i -b 32 -m resnet34
+python train.py -t t2i -b 32 -m resnet34
